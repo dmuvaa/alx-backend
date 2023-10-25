b5a85377d4a1e0b76987b45e6353b64e73437e1c
Dennis Muvaa <dmuvaa70@gmail.com>
2023-10-25 17:56:16 +0300

    Caching With the LIFO algorithm

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -18,8 +18,9 @@ class LIFOCache(BaseCaching):
         """func that inserts items into cache with LIFO strategy"""
         if key is None or item is None:
             return
-        
-        if len(self.cache_data) >= self.MAX_ITEMS and key not in self.cache_data:
+
+        if len(self.cache_data) >= \
+                self.MAX_ITEMS and key not in self.cache_data:
             discarded_key = self.order_keys.pop(-1)
             del self.cache_data[discarded_key]
             print("DISCARD: {}".format(discarded_key))
@@ -34,4 +35,3 @@ class LIFOCache(BaseCaching):
         if key is None:
             return None
         return self.cache_data.get(key, None)
-                
