11970f0d5a07c9444f1c5b50641ea89ba6c327d0
Dennis Muvaa <dmuvaa70@gmail.com>
2023-10-27 23:00:17 +0300

    Hypermedia Pagination

--- 0x00-pagination/2-hypermedia_pagination.py
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -15,6 +15,7 @@ class Server:
     DATA_FILE = "Popular_Baby_Names.csv"
 
     def __init__(self):
+        """Initialize dataset"""
         self.__dataset = None
 
     def dataset(self) -> List[List]:
@@ -27,6 +28,7 @@ class Server:
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """get page"""
         assert isinstance(page, int) and page > 0
         assert isinstance(page_size, int) and page_size > 0
 
