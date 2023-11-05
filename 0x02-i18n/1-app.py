fefd05354ef4c74e18688d8aea889d48d4fb7542
Dennis Muvaa <dmuvaa70@gmail.com>
2023-11-05 23:57:42 +0300

    Basic Babel setup

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+"""import modules"""
+
+from flask import Flask, render_template
+from flask_babel import Babel
+
+app = Flask(__name__)
+babel = Babel(app)
+
+class Config:
+    """babel config class"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCAL = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app.config.from_object(Config)
+
+
+@app.route('/')
+def index():
+    """create a python app"""
+    return render_template('1-index.html')
+
+if __name__ == '__main__':
+    """main"""
+    app.run(debug=True)
