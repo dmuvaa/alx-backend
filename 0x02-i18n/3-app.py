f4e24756886d65433d8bbe8e92d370c89875c325
Dennis Muvaa <dmuvaa70@gmail.com>
2023-11-06 01:08:25 +0300

    Parametrize templates

---
+++ 0x02-i18n/3-app.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+"""import requests"""
+
+from flask import Flask, render_template
+from flask_babel import Babel, _
+
+
+app = Flask(__name__)
+babel = Babel(app)
+
+
+class Config:
+    """ config class to set babel"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCAL = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app.config.from_object(Config)
+
+
+@app.route("/")
+def home():
+    """an endpoint to the index page"""
+    return render_template("2-index.html")
+
+
+if __name__ == "__main__":
+    app.run()
