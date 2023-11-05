7be51ee3dc914dc04d8907aa05aec5d896becefe
Dennis Muvaa <dmuvaa70@gmail.com>
2023-11-05 18:46:22 +0300

    Create Basic App

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -10,7 +10,7 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     """create a python app"""
-    return render_template('index.html')
+    return render_template('0-index.html')
 
 if __name__ == '__main__':
     """main"""
