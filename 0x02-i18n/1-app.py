fbbb12e13fa09b4ad5925d7a66150c3ad16da76f
Dennis Muvaa <dmuvaa70@gmail.com>
2023-11-06 00:11:38 +0300

    Basic Babel setup

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -11,7 +11,7 @@ babel = Babel(app)
 class Config:
     """babel config class"""
     LANGUAGES = ["en", "fr"]
-    BABEL_DEFAULT_LOCAL = "en"
+    BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"
 
 
